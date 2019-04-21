7fcc6cacce9ea6db02d133a59fe1ae01708cd9da
Giang Pham <conmeobeo96@gmail.com>
2019-04-21 17:22:57 +0300

    Introduce selectable tab

---
+++ colorchangeableindicator/src/main/java/leo/me/la/colorchangeableindicator/AnimatedCircleView.kt
@@ -0,0 +1,123 @@
+package leo.me.la.colorchangeableindicator
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import android.graphics.PointF
+import android.graphics.Canvas
+import android.graphics.Color
+
+
+private const val defaultRadius = 30
+
+internal class AnimatedCircleView : View {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val circle = Circle(paint)
+    private var animator: ValueAnimator? = null
+    private var _isEnabled = false
+
+    fun enable(enable: Boolean) {
+        if (enable) {
+            if (_isEnabled)
+                return
+            _isEnabled = true
+            paint.run {
+                style = Paint.Style.FILL
+                color = selectedColor
+            }
+            ValueAnimator.ofInt(0, radius)
+                .setDuration(300)
+                .apply {
+                    addUpdateListener {
+                        circle.currentRadius = it.animatedValue as Int
+                        invalidate()
+                    }
+                }
+                .also {
+                    animator = it
+                }
+                .start()
+        } else {
+            _isEnabled = false
+            paint.run {
+                style = Paint.Style.STROKE
+                strokeWidth = 5f
+                color = disableColor
+            }
+            animator?.cancel()
+            circle.currentRadius = radius - paint.strokeWidth.toInt() /2
+            invalidate()
+        }
+    }
+
+    var radius: Int = defaultRadius
+        set(value) {
+            require(value >= 0) { "Negative radius is forbidden" }
+            field = value
+            invalidate()
+        }
+    var selectedColor: Int = enableColor
+        set(value) {
+            field = value
+            if (_isEnabled) {
+                paint.color = value
+                invalidate()
+            }
+        }
+
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+        : super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val desiredHeight = 2 * radius + 2
+        val desiredWidth = 2 * radius + 2
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        val height = when (heightMode) {
+            MeasureSpec.EXACTLY -> heightSize
+            MeasureSpec.AT_MOST -> Math.min(desiredHeight, heightSize)
+            else -> desiredHeight
+        }
+        val width = when (widthMode) {
+            MeasureSpec.EXACTLY -> widthSize
+            MeasureSpec.AT_MOST -> Math.min(desiredWidth, widthSize)
+            else -> desiredWidth
+        }
+        setMeasuredDimension(width, height)
+        circle.setCenter(width / 2f, height / 2f)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        circle.draw(canvas, Math.min(width, height) / 2f)
+    }
+
+    inner class Circle(private val paint: Paint) {
+
+        private val center = PointF()
+
+        var currentRadius: Int = defaultRadius
+
+        fun setCenter(x: Float, y: Float) {
+            center.set(x, y)
+        }
+
+        fun draw(canvas: Canvas, limitedRadius: Float) {
+            canvas.drawCircle(
+                center.x,
+                center.y,
+                Math.min(currentRadius.toFloat(), limitedRadius),
+                paint
+            )
+        }
+    }
+}
---
+++ colorchangeableindicator/src/main/java/leo/me/la/colorchangeableindicator/SelectableTab.kt
@@ -0,0 +1,64 @@
+package leo.me.la.colorchangeableindicator
+
+import android.content.Context
+import android.graphics.Color
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+
+val disableColor = Color.parseColor("#C9B9B9B9")
+val enableColor = Color.parseColor("#2F2969")
+
+internal class SelectableTab(context: Context) : LinearLayout(context) {
+
+    private var textView: TextView
+    private var circle: AnimatedCircleView
+
+    private var _isEnabled = false
+
+    var selectedColor: Int = enableColor
+        set(value) {
+            field = value
+            circle.selectedColor = value
+            if (_isEnabled) {
+                textView.setTextColor(selectedColor)
+            }
+        }
+
+    var text: String
+        get() = textView.text.toString()
+        set(value) {
+            textView.text = value
+        }
+
+    var textSize: Float
+        get() = textView.textSize
+        set(value) {
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, value)
+        }
+
+    var circleRadius: Int
+        get() = circle.radius
+        set(value) {
+            circle.radius = value
+        }
+
+    init {
+        View.inflate(context, R.layout.selectable_tab, this)
+        gravity = Gravity.CENTER
+        orientation = VERTICAL
+        textView = findViewById(R.id.textview)
+        circle = findViewById(R.id.circle)
+    }
+
+    fun enable(enable: Boolean) {
+        circle.enable(enable)
+        if (enable) {
+            textView.setTextColor(selectedColor)
+        } else {
+            textView.setTextColor(disableColor)
+        }
+    }
+}
