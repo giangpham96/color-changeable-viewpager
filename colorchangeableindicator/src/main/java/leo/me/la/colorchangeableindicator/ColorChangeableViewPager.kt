4f904bfd81dedbe937fe19a7a529c32414cd03e4
Giang Pham <conmeobeo96@gmail.com>
2019-04-22 08:11:51 +0300

    Introduce color changeable viewpager

---
+++ colorchangeableindicator/src/main/java/leo/me/la/colorchangeableindicator/ColorChangeableViewPager.kt
@@ -0,0 +1,46 @@
+package leo.me.la.colorchangeableindicator
+
+import android.animation.ArgbEvaluator
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import androidx.viewpager.widget.ViewPager
+
+private val defaultViewPagerBackgroundColors = listOf(
+    Color.parseColor("#5545E482"),
+    Color.parseColor("#55F9B72B"),
+    Color.parseColor("#5543D6C5")
+)
+class ColorChangeableViewPager : ViewPager, ColorChangeable {
+    private val colorEvaluator = ArgbEvaluator()
+
+    var indicatorColors: List<Int> = defaultViewPagerBackgroundColors
+        set(value) {
+            field = value
+            setBackgroundColor(value[currentItem % indicatorColors.size])
+        }
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+        super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+        val nextPage = position + 1
+
+        val currentPageColor = indicatorColors[position % indicatorColors.size]
+        val nextPageColor = indicatorColors[nextPage % indicatorColors.size]
+
+        val color = colorEvaluator.evaluate(positionOffset, currentPageColor, nextPageColor) as Int
+        onColorChangedWhileScrolling(color, position, positionOffset, positionOffsetPixels)
+    }
+
+    override fun onColorChangedWhileScrolling(
+        color: Int,
+        position: Int,
+        positionOffset: Float,
+        positionOffsetPixels: Int
+    ) {
+        setBackgroundColor(color)
+    }
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+}
