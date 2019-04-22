e3c6b54f367cf3d92e9899a10d626d967876712b
Giang Pham <conmeobeo96@gmail.com>
2019-04-22 13:37:00 +0300

    Introduce offer mark

--- app/src/main/java/leo/me/la/indicator/PlaceHolderFragment.kt
+++ app/src/main/java/leo/me/la/indicator/PlaceHolderFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_place_holder.offer
 import kotlinx.android.synthetic.main.fragment_place_holder.textview
 
 private const val ARG_TEXT = "text"
@@ -28,6 +29,18 @@ class PlaceHolderFragment private constructor() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         textview.text = text
+        offer.post {
+            val w = offer.measuredWidth
+            val h = offer.measuredHeight
+            val sqrt2 = Math.sqrt(2.0).toFloat()
+            offer.animate()
+                .apply {
+                    duration = 0
+                }
+                .translationY((w - (1 + sqrt2) * h) / (2 * sqrt2))
+                .translationX(((sqrt2 - 1) * w + h) / (2 * sqrt2))
+                .rotation(45f)
+        }
     }
 
     companion object {
